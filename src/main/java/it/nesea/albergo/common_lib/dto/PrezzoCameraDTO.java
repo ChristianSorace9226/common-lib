4800b9004dcd386c292c8b24457e39057ab42052
Claca06 <clauscal.cc@gmail.com>
2024-12-19 14:29:26 +0100

    aggiunto PrezzoCameraDTO

---
+++ src/main/java/it/nesea/albergo/common_lib/dto/PrezzoCameraDTO.java
@@ -0,0 +1,26 @@
+package it.nesea.albergo.common_lib.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrezzoCameraDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3916634733009689102L;
+
+    private Integer idTipo;
+    private BigDecimal prezzoTotale;
+    private List<BigDecimal> prezziAPersona;
+    private Integer numeroOccupanti;
+
+}
+
