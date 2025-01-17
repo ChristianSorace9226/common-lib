83eb020e325ac6d658b75ae2af2fdd3b9183e77d
Claca06 <clauscal.cc@gmail.com>
2025-01-17 16:20:54 +0100

    aggiunta InfoPrenotazione

---
+++ src/main/java/it/nesea/albergo/common_lib/dto/InfoPrenotazione.java
@@ -0,0 +1,26 @@
+package it.nesea.albergo.common_lib.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class InfoPrenotazione implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1565774617114581004L;
+
+    Integer idUtente;
+    BigDecimal prezzoTotale;
+    Integer idMetodoPagamento;
+
+}
