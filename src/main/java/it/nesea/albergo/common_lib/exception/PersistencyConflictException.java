3ed2d14735c60219952b7f5a4c3958f3b48834af
ChristianSorace9226 <soracechristian0@gmail.com>
2025-01-23 10:10:18 +0100

    feature add PersistencyConflictException

---
+++ src/main/java/it/nesea/albergo/common_lib/exception/PersistencyConflictException.java
@@ -0,0 +1,11 @@
+package it.nesea.albergo.common_lib.exception;
+
+
+public class PersistencyConflictException extends RuntimeException {
+    public PersistencyConflictException(String msg) {
+        super(msg);
+    }
+
+
+
+}
